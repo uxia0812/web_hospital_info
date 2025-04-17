0b4062fb25344120479e6abb38a39954edbcccc3
uxia0812 <ujeong08112@biconnect.me>
2025-04-17 18:58:02 +0900

    Modify Footer App Download Button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,11 +9,17 @@ function Footer() {
         지금 바로 다운받아 보세요!
       </p>
       <div className="download-buttons">
-        <button className="ios-button">
+        <button 
+          className="ios-button"
+          onClick={() => window.open("https://apps.apple.com/kr/app/%ED%94%84%EB%A6%AC%ED%8C%85/id6504740710", "_blank")}
+        >
           <img src="/images/apple.png" alt="Apple" className="button-icon" />
           Download for iOS
         </button>
-        <button className="android-button">
+        <button 
+          className="android-button"
+          onClick={() => window.open("https://play.google.com/store/apps/details?id=com.biconnect.bps", "_blank")}
+        >
           <img src="/images/google.png" alt="Android" className="button-icon" />
           Download for Android
         </button>
@@ -26,3 +32,6 @@ function Footer() {
 }
 
 export default Footer;
+
+
+
